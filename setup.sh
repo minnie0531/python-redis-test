dc6504e6079fbb327a21d8b99fe0b884665661ad
minnie0531 <minheekim0531@gmail.com>
2021-09-06 10:32:22 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #/bin/bash
 
-# Set up vitualenv
+# Check if windows or linux
+
+os=`uname -s`
+echo $os
+
+echo "Current system is Linux"
+echo "Install vitual environment"
 python3 -m venv venv
 source ./venv/bin/activate
-
 pip3 install -r requirements.txt
+cp hooks/pre-commit.py .git/hooks/pre-commit
